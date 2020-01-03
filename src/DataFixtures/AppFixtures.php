8e153fc3b566c79247ed2df77bf8afdb8fda6cce
malick52 <malickaly52@gmail.com>
2020-01-03 15:10:20 +0000

    creation de fixture

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,32 @@
 
 namespace App\DataFixtures;
 
+
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
 
 class AppFixtures extends Fixture
 {
+    private $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
     public function load(ObjectManager $manager)
     {
-         $user = new User();
-        $manager->persist($product);
+         $user = new User("supadmin");
+         $user->setUsername("malick");
+         $user->setNom("Aly");
+         $user->setPassword($this->encoder->encodePassword($user, "supadmin"));
+         $user->setRoles(array("ROLE_SUPADMIN"));
+         $user->setIsActive(true);
 
-        $manager->flush();
+         $manager->persist($user);
+
+        $manager->flush  ();
     }
 }
